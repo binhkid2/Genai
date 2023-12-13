a42a9c770c27e5f091aedb781030f4a041b08b33
Abdelrahman1999 <jokergen7@gmail.com>
2023-12-13 06:10:32 +0200

    steps

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -20,7 +20,7 @@ app.get('/api/text2image', async (req, res) => {
   
   if(style.length>0)await sdk.sdxlGenerate({
       prompt: prompt,
-      steps: 5,
+      steps: 50,
       style_preset: style
     })
       .then(({ data }) =>  url = ("https://images.prodia.xyz/" + data.job + ".png" ))
