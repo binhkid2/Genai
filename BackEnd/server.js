8356c0226616b55868327013cc8bf40da22c445e
Abdelrahman1999 <jokergen7@gmail.com>
2023-12-13 02:43:39 +0200

    model

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -19,7 +19,7 @@ app.get('/api/text2image', async (req, res) => {
   const style = req.query.style;
   
   await sdk.sdxlGenerate({
-      model:"juggernautXL_v45.safetensors [e75f5471]",
+      model:"dynavisionXL_0411.safetensors [c39cc051]",
       prompt: prompt,
       steps: 25,
       style_preset: style
