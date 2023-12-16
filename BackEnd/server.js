c2a73b13f2b4ebffbb3362b349383b23ee2c430e
Abdelrahman1999 <jokergen7@gmail.com>
2023-12-16 07:22:52 +0200

    deleted steps

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -21,15 +21,13 @@ app.get('/api/text2image', async (req, res) => {
   if(style.length>0)await sdk.sdxlGenerate({
       prompt: prompt,
       style_preset: style,
-      model:"juggernautXL_v45.safetensors [e75f5471]",
-      steps:40
+      model:"juggernautXL_v45.safetensors [e75f5471]"
     })
       .then(({ data }) =>  url = ("https://images.prodia.xyz/" + data.job + ".png" ))
       .catch(err => console.error(err));
   else await sdk.sdxlGenerate({
     prompt: prompt,
-    model:"juggernautXL_v45.safetensors [e75f5471]",
-    steps:40
+    model:"juggernautXL_v45.safetensors [e75f5471]"
   })
     .then(({ data }) =>  url = ("https://images.prodia.xyz/" + data.job + ".png" ))
     .catch(err => console.error(err));    
