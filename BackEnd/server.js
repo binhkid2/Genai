11daf67775121aa9583c665eb118836578947235
Abdelrahman1999 <jokergen7@gmail.com>
2023-12-17 05:43:15 +0200

    steps

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -22,12 +22,14 @@ app.get('/api/text2image', async (req, res) => {
       prompt: prompt,
       style_preset: style,
       model:"juggernautXL_v45.safetensors [e75f5471]",
+      steps: 30
     })
       .then(({ data }) =>  url = ("https://images.prodia.xyz/" + data.job + ".png" ))
       .catch(err => console.error(err));
   else await sdk.sdxlGenerate({
     prompt: prompt,
     model:"juggernautXL_v45.safetensors [e75f5471]",
+    steps: 30
   })
     .then(({ data }) =>  url = ("https://images.prodia.xyz/" + data.job + ".png" ))
     .catch(err => console.error(err));    
